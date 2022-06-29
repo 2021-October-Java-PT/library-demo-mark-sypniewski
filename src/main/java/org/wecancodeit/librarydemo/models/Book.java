71761b581b92ea7984beb2dce1503de0864c6f1b
mark-sypniewski <msyp721@gmail.com>
2022-06-28 21:45:04 -0400

    1926

---
+++ src/main/java/org/wecancodeit/librarydemo/models/Book.java
@@ -0,0 +1,56 @@
+package org.wecancodeit.librarydemo.models;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    String title;
+    private String description;
+    @ManyToOne
+    private Campus campus;
+    @ManyToMany
+    private Collection<Author> authors;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Collection<Author> getAuthors() {
+        return authors;
+    }
+
+    public Book() {
+
+    }
+
+    public Book(String title, String description, Campus testCampus, Author... authors) {
+        this.title = title;
+        this.description = description;
+        this.campus = testCampus;
+        this.authors = new ArrayList<>(Arrays.asList(authors));
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/test/java/org/wecancodeit/librarydemo/JPAWiringTest.java
+++ src/test/java/org/wecancodeit/librarydemo/JPAWiringTest.java
@@ -5,6 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.wecancodeit.librarydemo.models.Author;
+import org.wecancodeit.librarydemo.models.Book;
+import org.wecancodeit.librarydemo.models.Campus;
+import org.wecancodeit.librarydemo.repositories.AuthorRepository;
+import org.wecancodeit.librarydemo.repositories.BookRepository;
+import org.wecancodeit.librarydemo.repositories.CampusRepository;
 
 import java.util.Optional;
 
@@ -41,8 +47,31 @@ public class JPAWiringTest {
 
         Optional<Campus> retrievedCampusOpt = campusRepo.findById(testCampus.getId());
         Campus retrievedCampus = retrievedCampusOpt.get();
-        Book retrievedBook = bookRepo.findById(testBook.getId()).get();
         assertThat(retrievedCampus.getBooks()).contains(testBook);
     }
 
+    @Test
+    public void booksShouldBeAbleToHaveMultipleAuthors() {
+        Campus testCampus = new Campus("Test Location");
+        Author testAuthor1 = new Author("Test firstName", "Test lastName");
+        Author testAuthor2 = new Author("Test firstName2", "Test lastName2");
+        Book testBook1 = new Book("Title", "Description", testCampus, testAuthor1, testAuthor2);
+        Book testBook2 = new Book("Title", "Description", testCampus, testAuthor2);
+        Book testBook3 = new Book("Title", "Description", testCampus, testAuthor1);
+
+        campusRepo.save(testCampus);
+        authorRepo.save(testAuthor1);
+        authorRepo.save(testAuthor2);
+        bookRepo.save(testBook1);
+        bookRepo.save(testBook2);
+        bookRepo.save(testBook3);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Book retrievedBook = bookRepo.findById(testBook1.getId()).get();
+        Author retrievedAuthor1 = authorRepo.findById(testAuthor1.getId()).get();
+        Author retrievedAuthor2 = authorRepo.findById(testAuthor2.getId()).get();
+        assertThat(retrievedBook.getAuthors()).contains(testAuthor1, testAuthor2);
+    }
 }
